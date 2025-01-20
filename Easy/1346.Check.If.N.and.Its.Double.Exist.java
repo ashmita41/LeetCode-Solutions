57ff792053f28d884ee08a48d24bc26c594f628f
Ashmita Pandey <97587859+ashmita41@users.noreply.github.com>
2025-01-20 19:30:02 +0530

    Create 1346.Check.If.N.and.Its.Double.Exist.java
    Added possible Solution

---
+++ Easy/1346.Check.If.N.and.Its.Double.Exist.java
@@ -0,0 +1,49 @@
+/*
+Given an array arr of integers, check if there exist two indices i and j such that : i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+ 
+Example 1:
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+
+Example 2:
+Input: arr = [3,1,7,11]
+Output: false
+Explanation: There is no i and j that satisfy the conditions.
+ 
+Constraints:
+2 <= arr.length <= 500
+-103 <= arr[i] <= 103
+*/
+// This is not the best solution because it can be also solved by HashSets which is a better method to solve this problem keeping complexity in mind
+
+// My Solution by Linear search
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (i != j && (arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+// Better Solution by Linear Search Only
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for (int i =0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                if (i!=j){
+                    
+                    if(arr[i]==2*arr[j]){
+                        return true;
+                    }
+                }
+                
+                
+            }
