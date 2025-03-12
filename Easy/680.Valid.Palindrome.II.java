08215be3ce90e7124e0bda47ed93b538bbff974f
Ashmita Pandey <97587859+ashmita41@users.noreply.github.com>
2025-03-12 21:44:58 +0530

    Create 680.Valid.Palindrome.II.java

---
+++ Easy/680.Valid.Palindrome.II.java
@@ -0,0 +1,48 @@
+/*
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+Example 1:
+Input: s = "aba"
+Output: true
+
+Example 2:
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+
+Example 3:
+Input: s = "abc"
+Output: false
+
+Constraints:
+1 <= s.length <= 105
+s consists of lowercase English letters.
+*/
+
+//Solution
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Skip either the left character or the right character and check for palindrome
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true; // If no mismatch was found, it's already a palindrome
+    }
+    
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
