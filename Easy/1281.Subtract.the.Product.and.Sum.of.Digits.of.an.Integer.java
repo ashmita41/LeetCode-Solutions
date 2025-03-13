c5d889b31751846d860f7bd382ee2ec13495cdbd
Ashmita Pandey <97587859+ashmita41@users.noreply.github.com>
2025-03-13 10:48:31 +0530

    Create 1281.Subtract.the.Product.and.Sum.of.Digits.of.an.Integer.java

---
+++ Easy/1281.Subtract.the.Product.and.Sum.of.Digits.of.an.Integer.java
@@ -0,0 +1,44 @@
+/*
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+Example 1:
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+
+Example 2:
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+ 
+Constraints:
+1 <= n <= 10^5
+*/
+
+//Solution 
+/* Complexity
+Time Complexity: O(d), where d is the number of digits in the number n. The loop runs once for each digit.
+Space Complexity: O(1), since we are using a constant amount of space for the sum and product.
+*/
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n = n / 10;
+        }
+
+        return product - sum;
+    }
+}
+
