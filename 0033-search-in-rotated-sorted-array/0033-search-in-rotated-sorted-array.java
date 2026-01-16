344261483e11368cf6c4f5c71184f2d48aba0fe5
Ashmita Pandey <97587859+ashmita41@users.noreply.github.com>
2026-01-16 10:01:38 +0530

    Time: 0 ms (100%), Space: 43.5 MB (92.97%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,43 +1,49 @@
 class Solution {
+
     public int search(int[] nums, int target) {
-        int pivot = -1;
+        int pivot = findPivot(nums, 0, nums.length - 1);
 
-        // find pivot
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i + 1] < nums[i]) {
-                pivot = i;
-                break;
-            }
-        }
-
-        // not rotated
         if (pivot == -1) {
             return binarySearch(nums, 0, nums.length - 1, target);
         }
 
-        // pivot itself
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-
-        // decide which side to search
         if (target >= nums[0]) {
-            return binarySearch(nums, 0, pivot - 1, target);
+            return binarySearch(nums, 0, pivot, target);
         } else {
             return binarySearch(nums, pivot + 1, nums.length - 1, target);
         }
     }
 
-    public static int binarySearch(int[] nums, int s, int e, int target) {
+    public int binarySearch(int[] nums, int s, int e, int target) {
         while (s <= e) {
             int m = s + (e - s) / 2;
 
             if (nums[m] == target) {
                 return m;
-            } else if (nums[m] > target) {
-                e = m - 1;
-            } else {
+            } else if (nums[m] < target) {
                 s = m + 1;
+            } else {
+                e = m - 1;
+            }
+        }
+        return -1;
+    }
+
+    public int findPivot(int[] nums, int s, int e) {
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+
+            if (m < e && nums[m] > nums[m + 1]) {
+                return m;
+            }
+            if (m > s && nums[m - 1] > nums[m]) {
+                return m - 1;
+            }
+
+            if (nums[s] <= nums[m]) {
+                s = m + 1;
+            } else {
+                e = m - 1;
             }
         }
         return -1;
