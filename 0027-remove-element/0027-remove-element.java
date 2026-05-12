3eba1d1850cd05f0b29965c697d9287251275d0d
Ashmita Pandey <97587859+ashmita41@users.noreply.github.com>
2026-05-12 08:39:30 +0530

    Time: 0 ms (100%), Space: 43 MB (97.22%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int j = nums.length - 1;
+        while(i <= j) {
+            if (nums[i] == val) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return i;
+    }
+}
